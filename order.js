c54d5b1099b45ce5c0a66aa2bc72a2489e67d2de
ThisisJackRyan <thisisjackryan1@gmail.com>
2022-11-15 20:46:38 -0600

    second push of second Draft

--- order.js
+++ order.js
@@ -1,60 +1,14 @@
-var expandedElement = null;
 
-
-/*
 window.onload = function thanks(){
     console.log("Shout out to Benton Diebold for alot on the java script for a privous project together");
 }
+/*var options = toToggle.querySelector(".options");
+  var checkout = toToggle.querySelector(".checkout");
+  options.classList.toggle("hidden");
+  checkout.classList.toggle("hidden");*/
+ 
 
-
-//NO WORK RIGHT NOW HAVE TO FIX FOR MY CODE I JUST COPIED IT FORM REPLIT!!!!!
-function onClothingClick(toToggle){
-    if(expandedElement == null){
-      toggleExpand(toToggle);
-    } else if(toToggle != expandedElement){
-      toggleExpand(expandedElement);
-      toggleExpand(toToggle);
-    }
-    //prevents overlapping onClicks from running
-    event.stopPropagation();
+  function expandItem(num){
+    var temp = "#dropDown" + num;
+    document.querySelector(temp).classList.toggle("hidden");
   }
-
-
-  function toggleExpand(toToggle) {
-    //toggles expanded class for toToggle
-    toToggle.classList.toggle("expanded");
-    //gets row of toToggle
-    row = toToggle.parentElement;
-    //gets all items in row of toToggle, including toToggle
-    toHide = row.children;
-  
-    //toggles hidden on all children that aren't toToggle
-    for (let i = 0; i < toHide.length; i++) {
-      // checks if current element is toToggle
-      if(toHide[i] == toToggle){
-        //updates expandedElement if toToggle is expanded
-        if(toHide[i].classList.contains("expanded")){
-          expandedElement = toHide[i];
-          price = toHide[i].querySelector(".price").innerHTML;
-          imgSRC = toHide[i].querySelector(".clothing-image").querySelector("img").src;
-        } else {
-          resetClothing(toHide[i]);
-          expandedElement = null;
-        }
-        //continues if current element is to toggle
-        continue;
-      } 
-      //toggles element hide if element isn't to toggle
-      toHide[i].classList.toggle("hidden");
-    }
-    //toggles options and checkout for toToggle
-    var options = toToggle.querySelector(".options");
-    var checkout = toToggle.querySelector(".checkout");
-    options.classList.toggle("hidden");
-    checkout.classList.toggle("hidden");
-    
-    //prevents overlapping onClicks from running
-    event.stopPropagation();
-  }
-*/
-    
